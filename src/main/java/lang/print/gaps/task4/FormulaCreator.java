89bbfecb0accc79464e648e7e34a4f8905e12c85
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-24 20:28:29 +0200

    initial commit

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        int formula = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(formula);
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+//        System.out.println((int)'a'+" "+(int)'r');
+
+        char letC = 99;
+        char letH = 104;
+        char letA = 97;
+        char letR = 114;
+
+        System.out.println(letC+" "+letH+" "+letA+" "+letR);
+
+
     }
 }
